86e17642fa485a7fc4a13951ada67160c2a60d85
monteirojmsjr <50029252+monteirojmsjr@users.noreply.github.com>
2019-10-29 14:15:52 -0300

    inicialização do projeto Pi2 Univesp

---
+++ P2Grupo4N21/Form1.Designer.cs
@@ -0,0 +1,165 @@
+﻿namespace P2Grupo4N21
+{
+    partial class UserControl1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(UserControl1));
+            this.labelTop = new System.Windows.Forms.Label();
+            this.labelTopUnder = new System.Windows.Forms.Label();
+            this.labelMensager = new System.Windows.Forms.Label();
+            this.buttonconfirma = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.timer3 = new System.Windows.Forms.Timer(this.components);
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.Editcpf1 = new System.Windows.Forms.MaskedTextBox();
+            this.SuspendLayout();
+            // 
+            // labelTop
+            // 
+            this.labelTop.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.labelTop.AutoSize = true;
+            this.labelTop.Enabled = false;
+            this.labelTop.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelTop.Location = new System.Drawing.Point(265, 68);
+            this.labelTop.Name = "labelTop";
+            this.labelTop.Size = new System.Drawing.Size(350, 55);
+            this.labelTop.TabIndex = 4;
+            this.labelTop.Text = "RECICLANDO";
+            this.labelTop.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // labelTopUnder
+            // 
+            this.labelTopUnder.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.labelTopUnder.AutoSize = true;
+            this.labelTopUnder.Enabled = false;
+            this.labelTopUnder.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelTopUnder.Location = new System.Drawing.Point(168, 138);
+            this.labelTopUnder.Name = "labelTopUnder";
+            this.labelTopUnder.Size = new System.Drawing.Size(583, 55);
+            this.labelTopUnder.TabIndex = 3;
+            this.labelTopUnder.Text = "GARRAFAS PLÁSTICAS";
+            this.labelTopUnder.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // labelMensager
+            // 
+            this.labelMensager.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.labelMensager.AutoSize = true;
+            this.labelMensager.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelMensager.Location = new System.Drawing.Point(123, 239);
+            this.labelMensager.Name = "labelMensager";
+            this.labelMensager.Size = new System.Drawing.Size(661, 55);
+            this.labelMensager.TabIndex = 2;
+            this.labelMensager.Text = "Entre com o número do CPF";
+            this.labelMensager.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // buttonconfirma
+            // 
+            this.buttonconfirma.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.buttonconfirma.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.buttonconfirma.Cursor = System.Windows.Forms.Cursors.Default;
+            this.buttonconfirma.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonconfirma.Location = new System.Drawing.Point(301, 433);
+            this.buttonconfirma.Name = "buttonconfirma";
+            this.buttonconfirma.Size = new System.Drawing.Size(292, 61);
+            this.buttonconfirma.TabIndex = 1;
+            this.buttonconfirma.Text = "buttonconfirma";
+            this.buttonconfirma.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.buttonconfirma.UseVisualStyleBackColor = false;
+            this.buttonconfirma.Click += new System.EventHandler(this.Buttonconfirma_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.Timer1_Tick);
+            // 
+            // timer2
+            // 
+            this.timer2.Tick += new System.EventHandler(this.Timer2_Tick);
+            // 
+            // timer3
+            // 
+            this.timer3.Tick += new System.EventHandler(this.Timer3_Tick);
+            // 
+            // serialPort1
+            // 
+            this.serialPort1.BaudRate = 115200;
+            // 
+            // Editcpf1
+            // 
+            this.Editcpf1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.Editcpf1.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Editcpf1.Location = new System.Drawing.Point(319, 342);
+            this.Editcpf1.Mask = "000,000,000-00";
+            this.Editcpf1.Name = "Editcpf1";
+            this.Editcpf1.Size = new System.Drawing.Size(245, 47);
+            this.Editcpf1.TabIndex = 0;
+            this.Editcpf1.ValidatingType = typeof(int);
+            // 
+            // UserControl1
+            // 
+            this.AcceptButton = this.buttonconfirma;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(913, 487);
+            this.Controls.Add(this.Editcpf1);
+            this.Controls.Add(this.buttonconfirma);
+            this.Controls.Add(this.labelMensager);
+            this.Controls.Add(this.labelTopUnder);
+            this.Controls.Add(this.labelTop);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "UserControl1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Reciclando";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.UserControl1_FormClosed);
+            this.Load += new System.EventHandler(this.UserControl1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelTop;
+        private System.Windows.Forms.Label labelMensager;
+        private System.Windows.Forms.Button buttonconfirma;
+        private System.Windows.Forms.Label labelTopUnder;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Timer timer3;
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.MaskedTextBox Editcpf1;
+    }
+}
+
---
+++ P2Grupo4N21/Form1.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace P2Grupo4N21
+{
+    public partial class UserControl1 : Form
+    {
+        int segundos;
+        string dados_serial;
+        //string texto="0";
+        
+        public void fecha_serial() // fecha porta serial
+        {
+            if (serialPort1.IsOpen == true)
+            {
+                serialPort1.BreakState = true;
+                serialPort1.DiscardInBuffer();
+                serialPort1.Close();
+            }
+        }
+        public void LimpaCpf()
+        {
+            Editcpf1.Clear();
+            timer1.Enabled = true;
+            timer1.Interval = 5000;
+            timer1.Start();
+        }
+
+        public UserControl1()
+        {
+            InitializeComponent();
+        }
+
+        private void UserControl1_Load(object sender, EventArgs e)
+        {
+            serialPort1.PortName ="COM3";
+            labelMensager.Text = "Entre com o número do CPF";
+            labelTop.Text = "RECICLANDO";
+            buttonconfirma.Text = "CONFIRMAR";
+        }
+
+        private void Buttonconfirma_Click(object sender, EventArgs e)
+        {
+            labelMensager.Text = "Entre com o número do CPF";
+            string cpf= Editcpf1.Text;
+            string ncpf = cpf;
+            
+            if (cpf == "111.111.111-11" || cpf == "222.222.222-22" || cpf == "333.333.333-33" || cpf == "444.444.444-44" || cpf == "555.555.555-55" || cpf == "666.666.666-66" || cpf == "777.777.777-77" || cpf == "888.888.888-88" || cpf == "999.999.999-99")
+            {
+                labelMensager.Text = "O número do CPF é Inválido !";                
+                LimpaCpf();
+            }
+            else
+            {
+                if (PI24N21.ValidaCPF.IsCpf(cpf))
+                {
+                    labelMensager.Text = "O número do CPF é Válido !";
+                    LimpaCpf();                   
+                    Editcpf1.Visible = false;
+                    timer1.Stop();
+                    timer2.Enabled = true;
+                    timer2.Interval = 2000;
+                    timer2.Start();
+                    segundos = 30;
+                    buttonconfirma.Visible = false;          
+                }
+                else
+                {
+                    labelMensager.Text = "O número do CPF é Inválido !";
+                    LimpaCpf();
+
+                }
+            }
+        }
+
+        private void Timer1_Tick(object sender, EventArgs e)
+        {
+            labelMensager.Text = "Entre com o número do CPF";
+            timer1.Stop();
+            timer1.Enabled = false;
+        }
+
+        private void Timer2_Tick(object sender, EventArgs e)
+        {
+            labelMensager.Text = "  INSIRA SUA GARRAFA";
+            timer3.Enabled = true;
+            timer3.Interval = 1000;
+            timer3.Start();            
+        }
+
+        private void Timer3_Tick(object sender, EventArgs e)
+        {
+            timer2.Stop();
+            timer2.Enabled = false;
+            if (segundos == 0)
+            {
+
+                timer3.Enabled = false;
+                segundos = 30;
+                labelMensager.ForeColor = Color.Black;
+                labelMensager.Text = "Entre com o número do CPF";
+                buttonconfirma.Visible = true;
+                
+                Editcpf1.Clear();
+                Editcpf1.Visible = true;
+               
+                
+                if (serialPort1.IsOpen == true)
+                {                        
+                    serialPort1.Write("1");
+                    fecha_serial();                                        
+                }
+
+            }
+            else
+            {
+                segundos--;
+                labelMensager.ForeColor = labelMensager.ForeColor == Color.Red ? Color.Black : Color.Red;
+                //labelMensager.ForeColor = Color.Black; 
+                labelMensager.Text = "INSIRA SUA GARRAFA " + segundos;     
+                if (serialPort1.IsOpen == false)
+                {
+                    serialPort1.Open();
+                    serialPort1.Write("2");
+                }
+
+                dados_serial= serialPort1.ReadExisting();       
+
+                if (dados_serial == "2")
+                {
+                    labelMensager.Text = "  GARRAFA RECEBIDA";
+                    segundos = 30;
+                }             
+            }
+        }
+        private void UserControl1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            fecha_serial();
+        }        
+    }
+}
