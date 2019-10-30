8910b2555429aef0e43b142b7f9d864e433f988f
Rodrigo de Avila <eng.comp.rodrigo.ao@gmail.com>
2019-10-30 00:40:57 -0300

    Adicionado a função de  verificação de abertura duplicada ao Program.cs comentada

--- P2Grupo4N21/Program.cs
+++ P2Grupo4N21/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Diagnostics; //USADA PARA ACESSO A FUNCIONALIDADES DE PROCESSOS DO WINDOWS.
 
 namespace P2Grupo4N21
 {
@@ -17,6 +18,16 @@ namespace P2Grupo4N21
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new UserControl1());
+
+            // INICIO DO ALGORITIMO DE VERIFICAÇÃO DE EXISTENCIA NO PROCESSO DO WINDOWS
+            Process aProcess = Process.GetCurrentProcess(); //cria um novo objeto sobre o processo do windows 
+            string aProcName = aProcess.ProcessName; //cria uma string com o nome do programa para comparação na lista de execulções do windows
+            if (Process.GetProcessesByName(aProcName).Length > 1)  //Verifica a existência do processo ja ativo e toma a descição
+            { 
+                MessageBox.Show("O programa já está em execução!", "Atenção", MessageBoxButtons.OK, MessageBoxIcon.Error); //Apresenta a mensagem para o usuario informando que o mesmo ja foi solicitado e esta em processo de abertura
+                return; //Retorna ao processo principal de abertura do software primario.
+            }
+            // FIM DO ALGORITIMO DE VERIFICAÇÃO DE EXISTENCIA NO PROCESSO DO WINDOWS
         }
     }
 }
